87ee8b90130a60dffb5cac4cafe6acfa66c1c56f
Dmitry Sherbakov <dvsherbakov@outlook.com>
2019-03-04 19:59:50 +0500

    индикация pyro датчика

--- LabProg/LaserHelper.cs
+++ LabProg/LaserHelper.cs
@@ -11,11 +11,13 @@ namespace LabProg
         private void LaserPortOn(object sender, RoutedEventArgs e)
         {
             _pyroSerial.OpenPort();
+            _pyroTimer.Start();
         }
 
         private void LaserPortOff(object sender, RoutedEventArgs e)
         {
             _pyroSerial.ClosePort();
+            _pyroTimer.Stop();
         }
 
     }
