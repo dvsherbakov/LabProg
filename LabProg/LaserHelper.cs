a85736b0fbec0332a52eb576dc7dd11761f2103f
Dmitry Sherbakov <dvsherbakov@outlook.com>
2019-03-04 16:53:07 +0500

    pyro and laser build structure

---
+++ LabProg/LaserHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LabProg
+{
+
+    public partial class MainWindow : Window
+    {
+
+        private void LaserPortOn(object sender, RoutedEventArgs e)
+        {
+            _pyroSerial.OpenPort();
+        }
+
+        private void LaserPortOff(object sender, RoutedEventArgs e)
+        {
+            _pyroSerial.ClosePort();
+        }
+
+    }
+}
---
+++ LabProg/LaserSerial.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Ports;
+
+namespace LabProg
+{
+    class LaserSerial
+    {
+        private readonly SerialPort _mPort;
+        private bool active = false;
+
+        public LaserSerial(string portStr)
+        {
+           
+            if (portStr == "") portStr = "COM2";
+            _mPort = new SerialPort(portStr)
+            {
+                BaudRate = 500000,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 8,
+                Handshake = Handshake.None,
+                RtsEnable = true
+            };
+
+        }
+
+    }
+}
