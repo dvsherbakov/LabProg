279a771cd82995806979b98e03ec8d14020fb784
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-12-01 09:12:50 +0500

    new project wpf

---
+++ LabControl/ViewModels/MainModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LabControl.ViewModels
+{
+    class MainModel : ViewModel
+    {
+        private WindowState f_curWindowState;
+        public WindowState CurWindowState
+        {
+            get => f_curWindowState;
+            set => Set(ref f_curWindowState, value);
+        }
+    }
+}
---
+++ LabControl/ViewModels/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LabControl.ViewModels
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string property = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(property);
+            return true;
+        }
+    }
+}
