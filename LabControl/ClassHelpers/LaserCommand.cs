82117e952a1d39b4211e3606833b81112267ef8e
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-12-25 18:36:08 +0500

    start include laser

---
+++ LabControl/ClassHelpers/LaserCommand.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabControl.ClassHelpers
+{
+    internal class LaserCommand
+    {
+        public readonly List<LCommand> CmdList = new List<LCommand>();
+
+        public LaserCommand()
+        {
+            CmdList.Add(new LCommand(1, "?GFw\r", "Get Firmware", "GFw"));
+            CmdList.Add(new LCommand(2, "?GFw|\r", "Get Firmware with splitter |", "GFw|"));
+            CmdList.Add(new LCommand(3, "?GFH\r", "Get Firmware Head", "GFH"));
+            CmdList.Add(new LCommand(4, "?GSN\r", "Get Serial Number", "GSN"));
+            CmdList.Add(new LCommand(5, "?GSH\r", "Get Serial Number Head", "GSH"));
+            CmdList.Add(new LCommand(6, "?GSI\r", "Get Spec Info", "GSI"));
+            CmdList.Add(new LCommand(7, "?GMP\r", "Get Maximum Power", "GMP"));
+            CmdList.Add(new LCommand(8, "?GWH\r", "Get Working Hours", "GWH"));
+            CmdList.Add(new LCommand(9, "?MDP\r", "Measure Diode Power", "MDP"));
+            CmdList.Add(new LCommand(10, "?MTD\r", "Measure Temperature diode", "MTD"));
+            CmdList.Add(new LCommand(11, "?MTA\r", "Measure Temperature ambient", "MTA"));
+            CmdList.Add(new LCommand(12, "?MTB\r", "Measure Temperature board", "MTB"));
+            CmdList.Add(new LCommand(13, "?GAS\r", "Get Actual Status", "GAS"));
+            CmdList.Add(new LCommand(14, "?GFB\r", "Get Falure Byte", "GFB"));
+            CmdList.Add(new LCommand(15, "?GLF\r", "Get Latched Failurе", "GLF"));
+            CmdList.Add(new LCommand(16, "?GLP\r", "Get Level Power value", "GLP"));
+            CmdList.Add(new LCommand(17, "?SLP0FA\r", "Set Level Power", "SLP"));
+            CmdList.Add(new LCommand(18, "?GPP\r", "Get Power setpoint as percentage", "GPP"));
+            CmdList.Add(new LCommand(19, "?SPP050\r", "Set Power as Percentage", "SPP"));
+            CmdList.Add(new LCommand(20, "?TPP070\r", "Temporary Power as Percentage", "TPP"));
+            CmdList.Add(new LCommand(21, "?GOM\r", "Get operating mode", "GOM"));
+            CmdList.Add(new LCommand(22, "?SOM0F\r", "Set operating mode", "SOM"));
+            CmdList.Add(new LCommand(23, "?SAP1\r", "Set auto power-up", "SAP"));
+            CmdList.Add(new LCommand(24, "?SAS1\r", "Set Auto Start", "SAS"));
+            CmdList.Add(new LCommand(30, "?TPP\r", "Get temp Power", "GPP"));
+            CmdList.Add(new LCommand(35, "?POn\r", "Power On", "POn"));
+            CmdList.Add(new LCommand(36, "?POf\r", "Power Off", "POf"));
+            CmdList.Add(new LCommand(37, "?LOn\r", "Laser On", "LOn"));
+            CmdList.Add(new LCommand(38, "?LOf\r", "Laser Off", "LOf"));
+
+
+        }
+
+        public LCommand GetCmdById(int id)
+        {
+            return CmdList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public LCommand SetPowerLvl(int lvl)
+        {
+            var pwr = (int)(lvl * 4095 / 800);
+            return new LCommand(40, $"?SLP{pwr:X3}\r", "Set Level Power", "SLP");
+        }
+
+        public int GetCommandByMem(string mem)
+        {
+            var res = 0;
+            foreach (var cmd in CmdList)
+            {
+                if (mem.Contains(cmd.SAnswer)) res = cmd.Id;
+            }
+
+            return res;
+        }
+    }
+    public class LCommand
+    {
+        public readonly int Id;
+        public readonly string Description;
+        public readonly string SCommand;
+        public readonly string SAnswer;
+
+        public LCommand(int i, string cmd, string descr, string ans)
+        {
+            Id = i;
+            Description = descr;
+            SCommand = cmd;
+            SAnswer = ans;
+        }
+    }
+}
---
+++ LabControl/PortModels/LaserSerial.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using LabControl.ClassHelpers;
+
+namespace LabControl.PortModels
+{
+    internal class LaserSerial
+    {
+        private readonly SerialPort _mPort;
+        private readonly LaserCommand _lCommand;
+        private static List<string> _errList;
+        private static List<string> _msgList;
+        private int _dPower;
+        private int DMaxPower;
+        private float TempAmb;
+        private int lPwr;
+        private int LaserType;
+
+        public LaserSerial(string portStr)
+        {
+            _errList = new List<string>();
+            _msgList = new List<string>();
+            if (portStr == "") portStr = "COM6";
+            _mPort = new SerialPort(portStr)
+            {
+                BaudRate = 9600,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 8,
+                Handshake = Handshake.None,
+                RtsEnable = true
+            };
+            _lCommand = new LaserCommand();
+            _mPort.DataReceived += DataReceivedHandler;
+            _dPower = 0;
+            DMaxPower = 0;
+
+        }
+
+        public void OpenPort()
+        {
+            _mPort.Open();
+            SendCommand(1);
+            SendCommand(7);
+            SendCommand(18);
+            SendCommand(16);
+            SendCommand(21);
+            SendCommand(13);
+            SendCommand(30);
+            SendCommand(23);
+        }
+
+        public void ClosePort()
+        {
+            if (_mPort.IsOpen) _mPort.Close();
+        }
+
+        public int GetDPower()
+        {
+            return _dPower;
+        }
+
+        public int GetMaxPower()
+        {
+            return DMaxPower;
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            var sp = (SerialPort)sender;
+            try
+            {
+                var cnt = sp.ReadBufferSize;
+                var mRxdata = new byte[cnt + 1];
+                try
+                {
+                    sp.Read(mRxdata, 0, cnt);
+                }
+                catch (Exception ex)
+                {
+                    _errList.Add(ex.Message);
+                }
+
+                var ascii = Encoding.ASCII;
+                var answrs = ascii.GetString(mRxdata).Split('\r');
+                foreach (var s in answrs)
+                {
+                    if ((s.Length > 3) && (s.Length < 50))
+                        _msgList.Add(s);
+                    SetResMatt(s);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Dispatcher.Invoke(() => windowLogBox.Items.Add(new LogBoxItem { Dt = DateTime.Now, LogText = "Приложение запущено" }));
+                _errList.Add(ex.Message);
+            }
+        }
+
+        private void SetResMatt(string res)
+        {
+            var cmd = _lCommand.GetCommandByMem(res);
+            switch (cmd)
+            {
+                case 7:
+                    Match mpwr = Regex.Match(res, @"([-+]?[0-9]*\.?[0-9]+)");
+                    if (mpwr.Success)
+                        DMaxPower = Convert.ToInt32(mpwr.Groups[1].Value);
+                    break;
+                case 9:
+                    var dpwr = Regex.Match(res, @"([-+]?[0-9]*\.?[0-9]+)");
+                    if (dpwr.Success)
+                        _dPower = Convert.ToInt32(dpwr.Groups[1].Value);
+                    break;
+                case 11:
+                    var tmpa = Regex.Match(res, @"([-+]?[0-9]*\.?[0-9]+)");
+                    if (tmpa.Success)
+                        TempAmb = Convert.ToInt32(tmpa.Groups[1].Value);
+                    break;
+                case 16:
+                    var lpwr = res.Substring(4);
+                    int b = Convert.ToInt32(lpwr, 16);
+                    lPwr = (int)(b * 800 / 4095);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SendCommand(int cmd)
+        {
+            if (_mPort.IsOpen)
+                _mPort.Write(_lCommand.GetCmdById(cmd).SCommand);
+        }
+
+        public void SetPower(int pwr)
+        {
+            if (this.LaserType == 0)
+            {
+                var cmd = _lCommand.SetPowerLvl(pwr);
+                if (_mPort.IsOpen)
+                {
+                    _mPort.Write(cmd.SCommand);
+                }
+            }
+            else SetPowerLevel(pwr);
+        }
+
+        public int GetLasePower()
+        {
+            return lPwr;
+        }
+
+        public void SetOn()
+        {
+            if (LaserType == 0)
+            {
+                SendCommand(35);
+                SendCommand(37);
+            }
+            else Start();
+        }
+
+        public void SetOff()
+        {
+            if (LaserType == 0)
+            {
+                SendCommand(36);
+                SendCommand(38);
+            }
+            else Stop();
+        }
+
+        private void Start()
+        {
+            if (LaserType == 1) SetPowerStart();
+            else
+            {
+                var cmd = new byte[8] { 0x53, 0x08, 0x06, 0x01, 0x00, 0x01, 0x63, 0x0D };
+                _mPort.Write(cmd, 0, 8);
+            }
+        }
+
+        private void Stop()
+        {
+            if (_mPort.IsOpen)
+            {
+                var cmd = new byte[8] { 0x53, 0x08, 0x06, 0x01, 0x00, 0x02, 0x64, 0x0D };
+                _mPort.Write(cmd, 0, 8);
+            }
+        }
+
+        public void SetPowerLevel(int level)
+        {
+            List<byte> command = new List<byte> { 0x53, 0x08, 0x04, 0x01 };
+            var bts = BitConverter.GetBytes(level);
+            command.Add(bts[1]);
+            command.Add(bts[0]);
+            Int16 sm = 0;
+            foreach (byte x in command)
+                sm += x;
+            var cb = BitConverter.GetBytes(sm);
+            command.Add(cb[0]);
+            command.Add(0x0D);
+
+            var cmd = command.ToArray();
+            _mPort.Write(cmd, 0, 8);
+        }
+
+        public void SetPowerStart()
+        {
+            List<byte> command = new List<byte> { 0x53, 0x08, 0x06, 0x01, 0x00, 0x01 };
+
+            Int16 sm = 0;
+            foreach (byte x in command)
+                sm += x;
+            var cb = BitConverter.GetBytes(sm);
+            command.Add(cb[0]);
+            command.Add(0x0D);
+
+            var cmd = command.ToArray();
+            _mPort.Write(cmd, 0, 8);
+        }
+
+        public void SetLaserType(int tp)
+        {
+            LaserType = tp;
+        }
+    }
+}
