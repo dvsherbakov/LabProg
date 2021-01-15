6338c471620027f639905bc9f0a6edb6f1c12ce5
Dmitry Sherbakov <dvsherbakov@gmail.com>
2021-01-15 16:49:56 +0500

    add dispencer serial

---
+++ LabControl/ClassHelpers/DispenserPulseWaveData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabControl.ClassHelpers
+{
+    internal class DispenserPulseWaveData
+    {
+        public int TimeRise1 { get; set; }
+        public int TimeT1 { get; set; }
+        public int TimeFall { get; set; }
+        public int TimeT2 { get; set; }
+        public int TimeRise2 { get; set; }
+        //Volts
+        public int V0 { get; set; }
+        public int V1 { get; set; }
+        public int V2 { get; set; }
+    }
+}
---
+++ LabControl/ClassHelpers/DispenserSineWaveData.cs
@@ -0,0 +1,10 @@
+﻿namespace LabControl.ClassHelpers
+{
+    internal abstract class DispenserSineWaveData
+    {
+
+        public int V0 { get; set; }
+        public int VPeak { get; set; }
+        public int TimeToverall { get; set; }
+    }
+}
